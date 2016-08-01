9cee5fcbe20e79c27d456ded6592cf7ef72ca3b4
Simas Skrebiškis <simas.skrebiskis@gmail.com>
2016-08-01 22:46:11 +0300

    django 1.10 support for urls

--- webtopay/urls/makro.py
+++ webtopay/urls/makro.py
@@ -1,11 +1,18 @@
 import django
 
-if django.VERSION >= (1, 6):
-    from django.conf.urls import patterns, url
+if django.VERSION < (1, 10):
+    if django.VERSION >= (1, 6):
+        from django.conf.urls import patterns, url
+    else:
+        from django.conf.urls.defaults import patterns, url
+
+    urlpatterns = patterns('webtopay.views',
+        url(r'^$', 'makro', name="webtopay-makro"),
+    )
 else:
-    from django.conf.urls.defaults import patterns, url
+    from django.conf.urls import url
+    from webtopay import views
 
-
-urlpatterns = patterns('webtopay.views',
-    url(r'^$', 'makro', name="webtopay-makro"),
-)
+    urlpatterns = [
+        url(r'^$', views.makro, name="webtopay-makro"),
+    ]
--- webtopay/urls/mikro.py
+++ webtopay/urls/mikro.py
@@ -1,11 +1,18 @@
 import django
 
-if django.VERSION >= (1, 6):
-    from django.conf.urls import patterns, url
+if django.VERSION < (1, 10):
+    if django.VERSION >= (1, 6):
+        from django.conf.urls import patterns, url
+    else:
+        from django.conf.urls.defaults import patterns, url
+
+    urlpatterns = patterns('webtopay.views',
+        url(r'^$', 'mikro', name="webtopay-mikro"),
+    )
 else:
-    from django.conf.urls.defaults import patterns, url
+    from django.conf.urls import url
+    from webtopay import views
 
-
-urlpatterns = patterns('webtopay.views',
-    url(r'^$', 'mikro', name="webtopay-mikro"),
-)
+    urlpatterns = [
+        url(r'^$', views.mikro, name="webtopay-mikro"),
+    ]
